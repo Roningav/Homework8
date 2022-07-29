013474be306d8cea6dabeea71cc0419d9601b10c
Alex <alex8224@mail.ru>
2022-07-29 22:48:12 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,96 @@
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+    string error = "Введите корректное число, пожалуйста!";
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else 
+        {
+            Console.Clear();
+            Console.WriteLine(error);
+        }
+    }
+
+    return result;
+}
+
+
+int[,] GetMatrix(int m, int n) 
+{
+    int[,] matrix = new int[m, n];
+    Random yuy = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+        matrix[i, j] = yuy.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int count = 0;
+void Sum(int[,] matrix)
+{
+    int[] array = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        { 
+            sum = sum+ matrix[i, j];
+        }
+        Console.WriteLine($"Сумма строки {i}: {sum}");
+        array[count] = sum;
+        count++;
+    }
+    int min = array[0];
+    for(count = 0; count<matrix.GetLength(0); count++)
+        {
+            if(min > array[count])
+            {
+                min = array[count];
+            }
+           
+        }
+        Console.Write($"Min: {min} ");
+}
+
+
+int m = GetNumber("Print m: ");
+int n = GetNumber("Print n: ");
+int[,] matrix = GetMatrix(m, n);
+PrintMatrix(matrix);
+Sum(matrix);
+
